4e55a757d388e57b826f0a807adc884be09582c5
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-19 14:48:43 +0530

    Initial commit

---
+++ app/src/main/java/e/wolfsoft1/media_liberty_uikit/liberty_media_account_main.java
@@ -0,0 +1,13 @@
+package e.wolfsoft1.media_liberty_uikit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class liberty_media_account_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.liberty_media_main);
+    }
+}
